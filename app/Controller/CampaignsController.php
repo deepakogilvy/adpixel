7a32038e33d8a9830769a4cf37e0f85cc82dafb9
shawetamittal <shaweta.mittal@ogilvy.com>
2016-12-08 15:26:05 +0530

    duplicate enteries

--- app/Controller/CampaignsController.php
+++ app/Controller/CampaignsController.php
@@ -1,4 +1,5 @@
 <?php
+
 App::uses( 'AppController', 'Controller' );
 
 class CampaignsController extends AppController {
@@ -9,7 +10,7 @@ class CampaignsController extends AppController {
     public $helpers = [ 'DataTable.DataTable' ];
 
     public function index() {
-
+        
     }
 
     public function add() {
@@ -28,16 +29,17 @@ class CampaignsController extends AppController {
                 if( is_uploaded_file( $this->request->params['form']['pixel_file']['tmp_name'] ) ) {
                     $filepath = $this->request->params['form']['pixel_file']['tmp_name'];
                     $filename = $this->request->params['form']['pixel_file']['name'];
-                    $pixel = $this->__extractPixelData( $campaign_id, $filepath, $filename );
-		    if( $pixel['valid_file'] ) {
-                        $this->Page->saveAll( $pixel['data'] );
-		    } else {
+                    $pixel = $this->__extractPixelData( $campaign_id, $filepath, $filename, 'add' );
+                    if( $pixel['valid_file'] ) {
+                        if(!empty($pixel['data']))
+                            $this->Page->saveAll( $pixel['data'] );
+                    } else {
                         $this->Campaign->id = $campaign_id;
                         $this->Campaign->delete();
                         $errorMessage = __( $pixel['error_message'] );
                         $this->Session->setFlash( $errorMessage, 'default', [ 'class' => 'danger' ] );
                         $this->redirect( [ 'controller' => 'campaigns', 'action' => 'index' ] );
-		    }
+                    }
                 } else {
                     $errorMessage = __( 'The pixel code was not uploaded, please try to upload again.' );
                 }
@@ -70,14 +72,15 @@ class CampaignsController extends AppController {
                 if( is_uploaded_file( $this->request->params['form']['pixel_file']['tmp_name'] ) ) {
                     $filepath = $this->request->params['form']['pixel_file']['tmp_name'];
                     $filename = $this->request->params['form']['pixel_file']['name'];
-                    $pixel = $this->__extractPixelData( $id, $filepath, $filename );
+                    $pixel = $this->__extractPixelData( $id, $filepath, $filename, 'edit' );
                     if( $pixel['valid_file'] ) {
-                        $this->Page->saveAll( $pixel['data'] );
+                        if(!empty($pixel['data']))
+                            $this->Page->saveAll( $pixel['data'] );
                     } else {
                         $errorMessage = __( $pixel['error_message'] );
                         $this->Session->setFlash( $errorMessage, 'default', [ 'class' => 'danger' ] );
                         $this->redirect( [ 'controller' => 'campaigns', 'action' => 'index' ] );
-                    }		    
+                    }
                 } else {
                     $errorMessage = __( 'The pixel code was not uploaded, please try to upload again.' );
                 }
@@ -101,24 +104,26 @@ class CampaignsController extends AppController {
         }
     }
 
-    protected function __extractPixelData( $campaign_id, $filepath, $filename ) {
-	copy( $filepath, WWW_ROOT . 'uploads/' . $filename );
+    protected function __extractPixelData( $campaign_id, $filepath, $filename, $type ) {
+        copy( $filepath, WWW_ROOT . 'uploads/' . $filename );
 
         $requiredHeader = [ 'Business Unit' => 'business_unit', 'Campaign' => 'campaign', 'Vendor' => 'vendor', 'Pixel Tag Name' => 'pixel_tag_name', 'Pixel ID Code' => 'pixel_id_code', 'Sizmek Activity ID / TTD Pixel Code' => 'pixel_code', 'Pixel Location/URL' => 'url' ];
         $validFile = false;
         $templateSheetName = 'PIXEL SHEET';
         $errorMessage = "";
+        $pixelCode = [];
         $uploadedFile = $filepath;
         $extension = explode( '.', $filename );
         $fileExtension = end( $extension );
         $sheetIndex = null;
-        $costerData = [ ];
-        $headerRow = [ ];
-        $requiredHeaderFound = [ ];
+        $costerData = [];
+        $duplicateCosterData = [];
+        $headerRow = [];
+        $requiredHeaderFound = [];
         if( in_array( $fileExtension, [ 'xlsx', 'xls' ] ) ) {
             $templateObj = $this->Excel->loadWorksheet( $uploadedFile );
             $sheetNames = $this->Excel->getSheetNames();
-            $invalid = [ ];
+            $invalid = [];
             if( !isset( $sheetNames[$templateSheetName] ) ) {
                 $errorMessage = __( 'The file was not uploaded because the sheet named PIXEL SHEET was not found. Please make sure that this sheet comes in the second position if you have multiple tabs, and try to upload again.' );
             } else {
@@ -137,7 +142,7 @@ class CampaignsController extends AppController {
                         }
                         $requiredHeaderFound = array_merge( array_intersect( $row, $requiredHeaderKeys ), $requiredHeaderFound );
                     } else {
-                        $temp = [ ];
+                        $temp = [];
                         foreach( $headerRow as $excelColumn => $column ) {
                             $column = isset( $requiredHeader[$column] ) ? $requiredHeader[$column] : $column;
                             $temp[$column] = $row[$excelColumn];
@@ -148,7 +153,7 @@ class CampaignsController extends AppController {
                             break;
                         }
                         if( count( array_filter( $temp ) ) > 0 ) {
-			    if( trim( $temp['campaign'] ) != '' ) {
+                            if( trim( $temp['campaign'] ) != '' ) {
                                 $temp['url'] = trim( $temp['url'] );
                                 $temp['pixel_code'] = trim( $temp['pixel_code'] );
                                 $temp['campaign_id'] = $campaign_id;
@@ -159,10 +164,29 @@ class CampaignsController extends AppController {
                                 $tempx['pixel_id_code'] = trim( $temp['pixel_id_code'] );
                                 $temp['row_data'] = json_encode( $tempx, true );
                                 $temp['data_row_number'] = $rowIndex;
-                                $costerData[] = $temp;
-			    }
+                                $arrayData[] = $temp;
+                                $costerData[trim( $temp['pixel_code'] ).'-'.$temp['url']] = $temp;
+                                if( $type == 'edit' )
+                                {
+                                    $links = $this->Page->find( 'all', [ 'fields' => [ 'id', 'pixel_code', 'url', 'row_data'], 'conditions' => [ 'pixel_code' => trim( $temp['pixel_code'] ), 'url' => trim( $temp['url'] ), 'campaign_id' => $campaign_id, 'is_deleted' => 0] ] );                              
+                                    if( !empty( $links ) )                                       
+                                        unset($costerData[trim( $temp['pixel_code'] ).'-'.$temp['url']]);
+                                }
+                                else{
+                                   $duplicateCosterData = array_filter($arrayData, function ($element) use ($costerData) {
+                                        return !in_array($element, $costerData);
+                                    });
+                                }
+                                
+                            }
                         }
                     }
+                }
+                $costerData = array_values($costerData);
+                if(!empty($duplicateCosterData)){
+                    $pixelCodeStr =  implode(",",array_column($duplicateCosterData,'pixel_code'));
+                    $errorMessage = __( "The file is not uploaded because of duplicate pixel codes i.e $pixelCodeStr " );
+                    $validFile = false;
                 }
                 if( empty( $headerRow ) ) {
                     $validFile = false;
@@ -174,7 +198,7 @@ class CampaignsController extends AppController {
             $errorMessage = __( 'The file is not uploaded because the file format is invalid: please save as .xls or .xlsx only, and try to upload again.' );
             $validFile = false;
         }
-        return ['file' => $filename, 'data' => $costerData, 'valid_file' => $validFile, 'found_header' => $requiredHeaderFound, 'required_header' => $requiredHeader, 'error_message' => $errorMessage, 'coster_position' => $sheetIndex ];
+        return [ 'file' => $filename, 'data' => $costerData, 'valid_file' => $validFile, 'found_header' => $requiredHeaderFound, 'required_header' => $requiredHeader, 'error_message' => $errorMessage, 'coster_position' => $sheetIndex ];
     }
 
     public function ajaxData() {
@@ -182,11 +206,11 @@ class CampaignsController extends AppController {
         $query = [ 'fields' => [ 'Campaign.name',
                 'Campaign.start_date', 'Campaign.end_date',
                 'Campaign.created', 'Campaign.id', 'Campaign.validation_week_days' ] ];
-        $sortByColumns = ['name', 'start_date', 'end_date', 'created' ];
-        $searchWordMap = [ ];
+        $sortByColumns = [ 'name', 'start_date', 'end_date', 'created' ];
+        $searchWordMap = [];
         $output = $this->TableSearch->data( $query, $sortByColumns, $searchWordMap );
         $data = $output['aaData'];
-        $output['aaData'] = [ ];
+        $output['aaData'] = [];
         $modelClass = $this->modelClass;
         if( $data ) {
             foreach( $data as $d ) {
@@ -198,7 +222,7 @@ class CampaignsController extends AppController {
                 }
                 $days = substr( $days, 0, -1 );
                 $deleted = '<a data-target="#deletePopup" data-toggle="modal" data-original-title="Delete Business Unit" type="button" data-id="' . $d[$modelClass]["id"] . '" class="btn btn-xs btn-danger delete-record glyphicon glyphicon-remove js-tt" ></a>';
-                $temp = $urlArray = [ ];
+                $temp = $urlArray = [];
                 $temp[] = $d[$modelClass]['name'];
                 $temp[] = date( 'M d, Y', strtotime( $d[$modelClass]['start_date'] ) );
                 $temp[] = date( 'M d, Y', strtotime( $d[$modelClass]['end_date'] ) );
@@ -215,13 +239,13 @@ class CampaignsController extends AppController {
 
     public function downloadCampaign( $param ) {
         $options = [ [ 'type' => 'left', 'table' => 'pages', 'alias' => 'page', 'conditions' => [ 'campaign.id = page.campaign_id' ] ],
-                     [ 'type' => 'left', 'table' => 'validation_logs', 'alias' => 'validation_log', 'conditions' => [ 'page.id = validation_log.page_id' ] ]
-                   ];
-        $campaignPage = $this->Campaign->find( 'all', ['fields' => [ 'Campaign.id', 'page.*', 'validation_log.*' ], 'conditions' => [ 'Campaign.id' => $param ] ,'joins' => $options ] );
+                [ 'type' => 'left', 'table' => 'validation_logs', 'alias' => 'validation_log', 'conditions' => [ 'page.id = validation_log.page_id' ] ]
+        ];
+        $campaignPage = $this->Campaign->find( 'all', [ 'fields' => [ 'Campaign.id', 'page.*', 'validation_log.*' ], 'conditions' => [ 'Campaign.id' => $param ], 'joins' => $options ] );
 
         $output = array();
-        foreach ( $campaignPage as $key => $value ) {
-            foreach ( $value as $tableKey => $tableData ) {
+        foreach( $campaignPage as $key => $value ) {
+            foreach( $value as $tableKey => $tableData ) {
                 if( $tableKey == 'page' ) {
                     $rowData = json_decode( $tableData['row_data'], true );
                     $output[$tableData['id']]['Business Unit'] = $rowData['business_unit'];
@@ -239,7 +263,7 @@ class CampaignsController extends AppController {
             }
         }
 
-        $output = array_values($output);
+        $output = array_values( $output );
 
         $this->Excel->createWorksheet();
         $this->Excel->setSheetName( $output[0]['Business Unit'] );
@@ -251,7 +275,7 @@ class CampaignsController extends AppController {
         $dataKeys = array_keys( $output[0] );
         $heading = array();
         $i = 0;
-        foreach ( $dataKeys as $head ) {
+        foreach( $dataKeys as $head ) {
             $heading[$i]['label'] = $head;
             $heading[$i]['width'] = '25';
             $heading[$i]['colorCode'] = 'DCDCDC';
@@ -263,8 +287,8 @@ class CampaignsController extends AppController {
         foreach( $output as $value ) {
             $row++;
             $k = 0;
-            foreach ( $heading as $labelValue ) {
-                $this->Excel->_activeSheet->setCellValueByColumnAndRow( $k , $row, $value[$labelValue['label']] );
+            foreach( $heading as $labelValue ) {
+                $this->Excel->_activeSheet->setCellValueByColumnAndRow( $k, $row, $value[$labelValue['label']] );
                 $k++;
             }
         }
@@ -272,7 +296,7 @@ class CampaignsController extends AppController {
         $this->Excel->output( 'Pixel code verification' . $campaignPage[0]['Campaign']['name'] . '.xlsx' );
     }
 
-    public function setHeading( $heading, $row, $options = [ ] ) {
+    public function setHeading( $heading, $row, $options = [] ) {
         if( isset( $options['height'] ) ) $this->Excel->setHeight( $row, $options['height'] );
         $heading = setOffset( $heading );
         foreach( $heading as $value ) {
@@ -280,4 +304,5 @@ class CampaignsController extends AppController {
         }
         $this->Excel->setHeaderStyle( $this->Excel->setStyleForHeader( $heading ), $row );
     }
+
 }
